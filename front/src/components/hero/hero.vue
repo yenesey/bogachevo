080423235777476badef3fb37a8bc1ab6bec2cb0
Denis Bogachev <denesey@gmail.com>
2019-03-11 22:26:54 +0300

    fix

--- front/src/components/hero/hero.vue
+++ front/src/components/hero/hero.vue
@@ -12,17 +12,17 @@
             a(href="#contact" v-smooth-scroll) Контакты
             a.disabled(href="#hobby" @click="clickOther") Прочее...
 
-    .flex.row
-      .xs.span-6.offset-2
-        .flex.column.greeting
-          span Привет!
-          span Меня зовут Денис
-          span Я создаю web приложения
+
+    .xs.span-6.offset-2
+      .flex.column.greeting
+        span Привет!
+        span Меня зовут Денис
+        span Я создаю web приложения
    
     .scroll
-        svg(width="40px" height="100%" viewBox="0 0 247 590" version="1.1"  xmlns="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink")
-          path#wheel(d="M123.359,79.775l0,72.843", style="fill:none;stroke:#fff;stroke-width:20px;")
-          path#mouse(d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z", style="fill:none;stroke:#fff;stroke-width:20px;")
+      svg(width="40px" height="100%" viewBox="0 0 247 590" version="1.1"  xmlns="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink")
+        path#wheel(d="M123.359,79.775l0,72.843", style="fill:none;stroke:#fff;stroke-width:20px;")
+        path#mouse(d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z", style="fill:none;stroke:#fff;stroke-width:20px;")
     
 </template>
 
@@ -90,7 +90,7 @@ export default {
 }
 
 header {
-  height:  6.125rem;
+  height: 6.125rem;
   @include transition(height .5s ease);
   position: fixed;
   z-index: 10;
@@ -127,8 +127,8 @@ header {
 
 .greeting {
   justify-content: center;
-  top:0;
-  bottom:0;
+  top: 89px;
+  height: 75vh;
   position: absolute;
   z-index: 2;
   color: #edf4ff;
@@ -180,7 +180,6 @@ svg #wheel {
   position: absolute;
   top: 78vh;
   margin-left: 80%;
-
 }
 //----------------------animate hello text---------------------------------
 
